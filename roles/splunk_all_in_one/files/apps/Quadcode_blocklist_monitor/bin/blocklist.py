7713ebb21c720584888da9b937f40bd776c82761
Filip Kuczak <filipkuczakit@gmail.com>
2023-06-22 22:00:27 +0000

    cleaned blocklist.py

--- roles/splunk_all_in_one/files/apps/Quadcode_blocklist_monitor/bin/blocklist.py
+++ roles/splunk_all_in_one/files/apps/Quadcode_blocklist_monitor/bin/blocklist.py
@@ -3,8 +3,8 @@ import time
 
 def get_blacklisted_ips(API_url):
     now = int(time.time())
-    five_minutes_ago = now - 3600
-    five_minutes_ago = str(five_minutes_ago)
+    one_hour_ago = now - 3600
+    one_hour_minutes_ago = str(five_minutes_ago)
     API_response = requests.get(API_url + five_minutes_ago)
     data = API_response.text
     return data
